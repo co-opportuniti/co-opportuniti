b6c8ce68ff12ef5a7ba95d89d4bf6f4e7a309619
duncanmayer <duncmayer@gmail.com>
2023-12-04 18:57:09 -0500

    changed display so that it's more accurate to fit the screen

--- screens/authentication/login.js
+++ screens/authentication/login.js
@@ -1,78 +1,87 @@
-
-import React, {useState} from 'react';
-import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 export default function LoginScreen() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-
-    async function handleLogin() {
-        if(email === "" || password === "") {
-            // where we would put a warning message to please enter data?
-            setError("Please enter login information.");
-            return;
-        }
-
-        const login = await handleCredentials(email, password);
-
+  async function handleLogin() {
+    console.log('Attempting to log in using credentials %s, %s', email, password);
+    // Add your login logic here
+    if (email === "" || password === "") {
         
-    };
+    }
+  }
 
-    return (
-        <View style = {styles.container}>  
-                <TextInput 
-                    style = {styles.inputField}
-                    value={email}
-                    onChangeText={text => setEmail(text)}
-                    placeholder = "Email"
-                    keyboardType = "email-address"
-                    autoCapitalize='none'
-                    />
-                <TextInput
-                    style={styles.inputField}
-                    value={password}
-                    onChangeText={text => setPassword(text)}
-                    placeholder = "Password"
-                    secureTextEntry
-                    />
-                <Button title="Login" onPress={handleLogin} style={styles.btn}/>
-
-            {/* figuring out how to display error message in login process? */}
-            <Text style = {styles.title}> {error} </Text> 
-        </View>
-    );
-};
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>CO</Text>
+     <Text style={styles.opportunitiText}>OPPORTUNITI</Text>
+      <View style={styles.contentContainer}>
+        <TextInput
+          style={styles.inputField}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          placeholder="Email"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.inputField}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          placeholder="Password"
+          secureTextEntry
+        />
+        <Button title="Login" color='white' onPress={handleLogin} style={styles.btn} />
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-        height:'100%',
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        padding: 16,
-        backgroundColor: 'gray',
-        borderRadius: 10
-    },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 20,
+  },
 
-    title: {
-        fontSize: 60, 
-        marginBottom: 20,
-        alignSelf: 'center'
-    },
-    inputField: {
-        height: 40, 
-        borderColor: 'black',
-        borderWidth:1,
-        paddingLeft:10,
-        marginBottom:10,
-        backgroundColor: 'white', // White background for text input
-        borderRadius: 5
-    },
-    btn: {
-        borderRadius: 5,
-        backgroundColor: 'gray', // Rounded gray background for the button
-        marginTop: 10
-    }
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    textAlign: 'center',
+  },
+
+  contentContainer: {
+    width: '80%',
+    backgroundColor: 'gray',
+    borderRadius: 10,
+    padding: 20,
+  },
+
+  inputField: {
+    height: 40,
+    width: 170,
+    borderColor: 'black',
+    borderWidth: 1,
+    paddingLeft: 10,
+    marginBottom: 10,
+    backgroundColor: 'white',
+    borderRadius: 5,
+  },
+
+  btn: {
+    borderRadius: 5,
+    marginTop: 10,
+    backgroundColor: 'white',
+  },
+  opportunitiText: {
+    fontSize: 24,
+    marginBottom: 30,
+    fontWeight: 'bold',
+    color: 'red',
+  },
 });
